99c92d339756de04712df5994dc4824d843fa877
Nancy Majors <nmajors@gmail.com>
2016-04-28 15:03:57 -0500

    gh-pages key

--- app/modules/products/service.js
+++ app/modules/products/service.js
@@ -2,15 +2,12 @@ class ProductService{
   constructor($q, $http){
     this._$q = $q;
     this._$http = $http;
-    this.key="MDoyYzI2NDM1Ni0wYzg0LTExZTYtYWQzYi1mM2JiZDQ0MDEyODM6M3BWZG13cFQzQWhJY3RzWFRzY0ZXR0JuOW91NVNiQnlBajVH";
-    // this.products = [];
+    // this.key="MDoyYzI2NDM1Ni0wYzg0LTExZTYtYWQzYi1mM2JiZDQ0MDEyODM6M3BWZG13cFQzQWhJY3RzWFRzY0ZXR0JuOW91NVNiQnlBajVH"; //localhost key
+    this.key="MDpmZWMxMmQ3NC0wZDdiLTExZTYtODQwZC1kNzhkMGIzNmJiMjQ6S2t2bmZTNWEzdzRZVlNnRmFaQUQwMUVZckNSVHJ1d3E3UFph"; //gh-pages key
+
   }
 
   all(){
-    // let key="MDoyYzI2NDM1Ni0wYzg0LTExZTYtYWQzYi1mM2JiZDQ0MDEyODM6M3BWZG13cFQzQWhJY3RzWFRzY0ZXR0JuOW91NVNiQnlBajVH";
-    console.log("in your all")
-    // return new this._$q((resolve, reject) => {
-
     return this._$http.get(`https://lcboapi.com/products?access_key=${this.key}`);
   }
 
